a124a117860b0d292b061dce609e7349595d2acf
Claudio Junior <claudio.1091@gmail.com>
2018-08-17 12:00:39 -0300

    Added Firebase 3.2

--- src/screens/BillList.js
+++ src/screens/BillList.js
@@ -3,6 +3,8 @@ import { View, Text, AsyncStorage } from 'react-native';
 import { Content, List } from 'native-base';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import firebase from 'react-native-firebase';
+
 import { STORAGE } from '../constants';
 import Container from '../components/Container';
 import FabButton from '../components/FabButton';
@@ -30,6 +32,42 @@ export default class BillList extends Component {
     const _this = this;
 
     didBlurSubscription = this.props.navigation.addListener('willFocus', () => _this.getFromStorage());
+  }
+
+  componentDidMount() {
+    firebase
+      .auth()
+      .signInAnonymouslyAndRetrieveData()
+      .then((credential) => {
+        if (credential) {
+          console.log('default app user ->', credential.user.toJSON());
+        }
+
+        const FCM = firebase.messaging();
+        const ref = firebase.firestore().collection('users');
+
+        // requests permissions from the user
+        FCM.requestPermissions();
+
+        FCM.onMessage((payload) => {
+          console.log('On Message ->', payload);
+        });
+
+        FCM.getInitialNotification().then((payload) => {
+          console.log('Inicial Notification ->', payload);
+        });
+
+        // gets the device's push token
+        FCM.getToken().then((token) => {
+          // stores the token in the user's document
+          console.log('token ->', token);
+          firebase
+            .firestore()
+            .collection('bills')
+            .doc('notification')
+            .update({ pushToken: token });
+        });
+      });
   }
 
   componentWillUnmount() {
@@ -62,6 +100,11 @@ export default class BillList extends Component {
       bills = [];
     }
 
+    const Banner = firebase.admob.Banner;
+    const AdRequest = firebase.admob.AdRequest;
+    const request = new AdRequest();
+    request.addKeyword('foobar');
+
     return (
       <Container>
         <Content>
@@ -84,6 +127,15 @@ export default class BillList extends Component {
         </Content>
 
         <FabButton navigation={this.props.navigation} />
+
+        <Banner
+          unitId="ca-app-pub-3940256099942544/6300978111"
+          size="FULL_BANNER"
+          request={request.build()}
+          onAdLoaded={() => {
+            console.log('Advert loaded');
+          }}
+        />
       </Container>
     );
   }
